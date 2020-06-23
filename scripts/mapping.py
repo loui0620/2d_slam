3f4f12b8e2224a452c8d4663eaf35caeb93367c1
loui0620 <woodlica@gmail.com>
2020-06-23 23:17:23 +0900

    Remove unused dependencies

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from data_loader import DataLoader, DroneTrajectoryData, LidarSweepData
