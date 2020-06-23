63b4afc9a76454d79f7b977bb1afabc00da1499a
loui0620 <woodlica@gmail.com>
2020-06-23 18:52:23 +0900

    Update README

---
+++ scripts/data_loader.py
@@ -0,0 +1,104 @@
+import csv
+import os
+import numpy as np
+import matplotlib as pyplot
+
+class DataLoader():
+    def __init__(self):
+        self.path_ID = []
+        self.drone_pos = []
+        self.scan_ID = []
+        self.sweep_data = []
+        self.dic_sweep_data = {}
+
+    def readFlightPath(self, file_path):
+        with open(file_path, newline='') as csvfile:
+            rows = csv.reader(csvfile, delimiter=':')
+            read_cnt = 0
+
+            for row in rows:
+                if row :
+                    if read_cnt % 2 == 0:
+                        temp1 = int(row[0].strip().split(',')[0])
+                        # temp2 = int(row[0].strip().split(',')[1])   
+                        self.path_ID.append(temp1)
+                    else:
+                        temp1 = float(row[0].strip().split(',')[0])
+                        temp2 = float(row[0].strip().split(',')[1])   
+                        self.drone_pos.append([temp1, temp2])  
+
+                read_cnt += 1
+        
+        return np.array(self.path_ID), np.array(self.drone_pos)
+
+    def readLidarData(self, file_path):
+        with open(file_path, newline='') as csvfile:
+            rows = csv.reader(csvfile, delimiter=':')
+            
+            read_cnt = 0
+            set_cnt = 0
+            
+            head_idx = 0
+            data_length = 0
+
+            read_open = False
+            sweep_temp = []
+
+            for row in rows:
+                
+                temp1 = row[0].strip().split(',')[0]
+                temp2 = row[0].strip().split(',')[1]
+                
+                if self.isRepresentInt(temp1):
+                    if int(temp1) == set_cnt:
+                        self.scan_ID.append(int(temp1))
+                        head_idx = read_cnt
+                        data_length = int(temp2)
+                        
+                        # print("set_cnt: {}".format(set_cnt))
+                        # print("head_index: {}".format(head_idx))
+                        # print("data_length: {}".format(temp2))
+                        # print("prev_data_size: {}".format(len(sweep_temp)))
+                        
+                        sweep_temp = []
+                        set_cnt += 1
+                        
+                    
+                else:
+                    if read_cnt <=  (head_idx + data_length):
+                        sweep_temp.append([float(temp1), float(temp2)])
+
+                        if read_cnt == head_idx + data_length:
+                            # print("line number: {}".format(read_cnt))
+                            # print("last data: {}, {}".format(float(temp1), float(temp2)))
+                            # print("last size data: {}\n".format(len(sweep_temp)))
+                            self.sweep_data.append(sweep_temp)
+
+
+                read_cnt += 1
+
+        return np.array(self.scan_ID), np.array(self.sweep_data)
+    
+    def isRepresentInt(self, s):
+        try: 
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+
+class DroneTrajectoryData(DataLoader):
+    def __init__(self, file_path):
+        self.pose_ID = None
+        self.drone_position = None
+        self.loader = DataLoader()
+        self.pose_ID, self.drone_position = self.loader.readFlightPath(file_path)
+
+
+class LidarSweepData(DataLoader):
+    def __init__(self, file_path):
+        self.scan_ID = None
+        self.sweep_data_raw = None
+        self.loader = DataLoader()
+        self.scan_ID, self.sweep_data_raw = self.loader.readLidarData(file_path)
+
---
+++ scripts/main.py
@@ -0,0 +1,165 @@
+import sys, getopt
+import os
+import math
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib.animation import FuncAnimation
+from data_loader import DataLoader, DroneTrajectoryData, LidarSweepData
+from optimizer import RRTSolver
+from dijkstra import Dijkstra
+from visualizer import Drawer
+from mapping import Mapping
+
+show_animation = True
+save_result = False
+save_path = os.path.dirname(os.path.abspath(__file__)) + "/../result/"
+
+def helper():
+    print("====================================================================")
+    print("This is assignment from Socville. Which contains functions included:\n\
+    1. Visualizing Drone Lidar Data .\n\
+    2. Mapping Lidar Data & save result.\n\
+    3. Path Reroouting. & save result")
+    print("====================================================================")
+    print("Augments:")
+    print("Assign drone data: --drone_data <drone_data_path>")
+    print("Assign lidar data: --lidar_data <lidar_data_path>")
+    print("Run visualizer: --run_visualize True")
+    print("Set Sweep ID to Visualizer: --data_to_show <scan_ID_list> i.e. [0,1,2,3]")
+    print("Save Result: --save_result")
+    print("\n")
+    print ('Usage Example: python scripts/main.py --path FlightPath.csv --lidar_data LIDARPoints.csv -n [0,1,2] --save true\n')
+
+
+def ask_user():
+    helper()
+    print("\nChoose Function to Run: (A) visualization (B) mapping (C) rerouting (D) all")
+    response = ''
+    while response.lower() not in {"a", "b", "c", "d"}:
+        response = input("Please enter above options: ")
+    return response.lower()
+
+
+def runRerouting(obstacle_list, start_grid, end_grid, is_save=False):
+    # start and goal position
+    sx = start_grid[0]  # [m]
+    sy = start_grid[1]  # [m]
+    gx = end_grid[0]  # [m]
+    gy = end_grid[1]  # [m]
+    grid_size = 0.5  # [m]
+    robot_radius = 0.5  # [m]
+
+    # set obstacle positions
+    ox, oy = [], []
+    path = []
+
+    for i in range(len(obstacle_list)):
+        ox.append(obstacle_list[i][0])
+        oy.append(obstacle_list[i][1])
+
+    if show_animation:  # pragma: no cover
+        plt.title("Rerouting")
+        plt.plot(ox, oy, ".k")
+        plt.plot(sx, sy, "og")
+        plt.plot(gx, gy, "xb")
+        plt.grid(True)
+        plt.axis("equal")
+
+    dijkstra = Dijkstra(ox, oy, grid_size, robot_radius)
+    rx, ry = dijkstra.planning(sx, sy, gx, gy)
+
+    for i in range(len(rx)):
+        path.append([i, 1])
+        path.append([rx[i], ry[i]])
+
+    if show_animation:  # pragma: no cover
+        plt.plot(rx, ry, "-r")
+        if is_save:
+            fig = plt.gcf()
+            fig.savefig(save_path + 'task3_rerouted.png')
+        plt.pause(0.01)
+        plt.show()
+        
+    return path
+
+
+def main(argv):
+    path_file = ''
+    lidar_file = ''
+    data_to_show = []
+    try:
+        opts, args = getopt.getopt(argv,"hp:d:n:s:",["path=","lidar_data=","show_data=", "save="])
+    except getopt.GetoptError:
+        print ('\nUsage Example: python scripts/main.py --path FlightPath.csv --lidar_data LIDARPoints.csv -n [0,1,2] --save true\n')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            helper()
+            sys.exit()
+        elif opt in ("-p", "--path"):
+            path_file = arg
+        elif opt in ("-d", "--lidar_data"):
+            lidar_file = arg
+        elif opt in ("-n", "--show_data"):
+            list_str = arg
+            data_to_show = list(map(int, list_str.strip('[]').split(',')))
+        elif opt in ("-s", "--save"):
+            is_save = arg
+            if is_save.lower() == 'true':
+                save_result = True
+            else:
+                save_result = False
+        else:
+            print ('\nUsage Example: python scripts/main.py --path FlightPath.csv --lidar_data LIDARPoints.csv -n [0,1,2]\n')
+            sys.exit()
+
+    drone_data = DroneTrajectoryData(path_file)
+    lidar_data = LidarSweepData(lidar_file)
+    
+    choice = ask_user()
+
+    # 1. Visualizing Task
+    if choice in {'a', 'd'}:
+        drawer = Drawer()
+        drawer.save_result = save_result
+        drawer.drawDronePath(drone_data)
+        drawer.drawLIDARPoints(lidar_data, drone_data, data_to_show=data_to_show, with_trace=True)
+    
+    # 2. Mapping Task
+    if choice in {'b', 'd'}:
+        map_mgr = Mapping()
+        map_mgr.save_result = save_result    
+        map_data = map_mgr.runMappingAndSave(lidar_data, drone_data, 'map.csv')
+        map_grid, obstacle_list = map_mgr.gridlizeMapData(map_data)
+        
+    # 3. Path Optimization Task
+    if choice in {'c', 'd'}: 
+        if choice == 'c':
+            map_mgr = Mapping()  
+            map_mgr.save_result = save_result    
+            map_data = map_mgr.runMappingAndSave(lidar_data, drone_data, 'map.csv')
+            map_grid, obstacle_list = map_mgr.gridlizeMapData(map_data)
+
+        start_grid = drone_data.drone_position[0] # 0-th
+        end_grid = drone_data.drone_position[-2] # 16-th
+
+        path = runRerouting(obstacle_list, start_grid, end_grid, save_result)
+
+        with open('ReroutedPath.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            cnt = 0
+            for i in range(len(path)):
+                if (i % 2) == 0:
+                    writer.writerow([int(i), int(1)])
+                else:
+                    writer.writerow([path[i][0], path[i][1]])
+
+    print("program finished.")
+
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
